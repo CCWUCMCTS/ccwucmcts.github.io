6ee64623e9de29453308064599c54730753e69fa
CCWUCMCTS <wwwwww931121@163.com>
2021-11-11 10:16:00 +0800

    Site updated: 2021-11-11 10:15:51

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -48,7 +48,7 @@
   jQuery(document).ready(function ($) {
     $("body").click(function (e) {
       var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
-      var colors = ["#000000","#292421","#C0C0C0","#F0FFF0","#FFFFCD","#FFF5EE","#FF0000","#FF4500","#FF7F50","#FFFF00","#FFD700","#FF8000","#33A1C9","#1E90FF","#0000FF","#87CEEB","#4169E1","#00FFFF","#00FF00","#7CFC00","#A020F0","#9933FA","#DA70D6","#BDFCC9"];
+      var colors = ["#000000","#292421","#FF0000","#FF4500","#FF7F50","#FFFF00","#FFD700","#FF8000","#33A1C9","#1E90FF","#0000FF","#87CEEB","#4169E1","#00FFFF","#00FF00","#7CFC00","#A020F0","#9933FA","#DA70D6","#BDFCC9"];
       var $i = $("<span/>").text(a[a_idx]);
       a_idx = (a_idx + 1) % a.length;
       var x = e.pageX,
